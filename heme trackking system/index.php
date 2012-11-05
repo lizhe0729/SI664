e3561d4cc7e1dff4934b869a74c01a4f889541e1
jack <lizhe0729@gmail.com>
2012-11-04 21:11:05 -0500

    three files

---
+++ heme trackking system/index.php
@@ -0,0 +1,24 @@
+<html>
+
+<head>
+  <link href="css/paper_style.css" rel="stylesheet" type="text/css"/>
+  <script src="js/jquery-1.8.2.min.js" language="javascript" type="text/javascript"></script>
+  <script src="js/jquery.maskedinput-1.3.js" language="javascript" type="text/javascript"></script>
+  <script src="js/main.js" language="javascript" type="text/javascript"></script>
+</head>
+
+<body>
+
+<table id = "Login">
+<tr><td id="Row1"><b>Hematopathology Case Tracking System</b></td></tr>
+<tr><td id="Row2">Accession Number: <input type="text" id="Number" size="35"></td></tr>
+<tr><td id="Row3"><a href="">Update Employee Information</a></td></tr>
+<tr><td id="Row3"><a href="">Update Institution Information</a></td></tr>
+<tr><td id="Row3"><a href="">Update Contacts Information</a></td></tr>
+<tr><td id="Row4"><a href="">Generate Report</a></td></tr>
+
+  
+</table>
+</body>
+
+</html>
---
+++ heme trackking system/js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+   $('#Number').mask("aa-99999");
+});
